8b1242d885073b2e1ccbb12c0de7bf55294eae5d
For3vigt <nickwelles@live.nl>
2024-10-04 10:57:08 +0200

    feat: :sparkles: Functionality to POST and GET comment(s)

--- src/comment/comments/views.py
+++ src/comment/comments/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Comments
+from .serializers import CommentSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def create_comment(request):
+    if request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CommentList(generics.ListAPIView):
+    queryset = Comments.objects.all() 
+    serializer_class = CommentSerializer  
