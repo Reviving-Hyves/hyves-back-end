1c5df4a4f37ecaaef4978518c85a224f6ac177f0
For3vigt <nickwelles@live.nl>
2024-10-08 17:08:14 +0200

    refactor: :hammer: Provide post_id functionality when posting a comment

--- src/comment/comments/urls.py
+++ src/comment/comments/urls.py
@@ -3,7 +3,7 @@ from .views import create_comment, CommentList
 
 
 urlpatterns = [
-    path('comment/', create_comment, name='create_comment'),
-    path('comments/', CommentList.as_view(), name='comment-list'),
+    path('comment/comment/<int:post_id>', create_comment, name='create_comment'),
+    path('comment/comments/', CommentList.as_view(), name='comment-list'),
 
 ]
