2c4fb5c8dff6467e7974dbc8852dfc458164211f
For3vigt <nickwelles@live.nl>
2024-10-09 12:24:57 +0200

    feat: :sparkles: Add DELETE & GET by post_id

--- src/comment/comment_service/settings.py
+++ src/comment/comment_service/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'comment_db.sqlite3',
     }
 }
 
--- src/comment/comments/urls.py
+++ src/comment/comments/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import create_comment, CommentList
+from .views import create_comment, delete_comment, comment_by_post_id, CommentList
 
 
 urlpatterns = [
-    path('comment/comment/<int:post_id>', create_comment, name='create_comment'),
-    path('comment/comments/', CommentList.as_view(), name='comment-list'),
+    path('comment/list/', CommentList.as_view(), name='comment_list'),
+    path('comment/<int:post_id>/comments/', comment_by_post_id, name='comment_by_post_id'),
+    path('comment/create/<int:post_id>', create_comment, name='create_comment'),
+    path('comment/delete/<int:comment_id>', delete_comment, name='delete_comment'),
 
 ]
