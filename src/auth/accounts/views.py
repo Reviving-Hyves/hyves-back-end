d4b1fb76c328da8ba85cc8e03785b6526e9d3546
For3vigt <nickwelles@live.nl>
2024-10-19 10:45:42 +0200

    feat: :sparkles: Add generic CRUD endpoints to Auth microservice

--- src/auth/accounts/views.py
+++ src/auth/accounts/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import UserSerializer, MyTokenObtainPairSerializer
 
@@ -21,3 +24,53 @@ class RegisterView(generics.CreateAPIView):
 # Login view to get JWT token
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+# View to get account info
+@api_view(['GET'])
+def get_account_info(request):
+    user = request.user
+    
+    try:
+        user = User.objects.get(pk=user.id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response({"error": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+# Delete account
+@api_view(['DELETE'])
+def delete_account(request):
+    user = request.user
+    password = request.data.get('password')
+
+    if not password:
+        return Response({"error": "Password is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+    if not user.check_password(password):
+        return Response({"error": "Incorrect password"}, status=status.HTTP_403_FORBIDDEN)
+
+    user.delete()
+    return Response({"message": "Account deleted successfully"}, status=status.HTTP_200_OK)
+
+# Update user profile data
+@api_view(['PUT'])
+def update_account(request):
+    user = request.user
+    email = request.data.get('email', user.email)
+    
+    try:
+        validate_email(email)
+    except ValidationError:
+        return Response({"error": "Invalid email format"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user.username = request.data.get('username', user.username)
+    user.first_name = request.data.get('first_name', user.first_name)
+    user.last_name = request.data.get('last_name', user.last_name)
+    user.email = email
+
+    try:
+        user.save()
+        return Response({"message": "Account updated successfully"}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({"error": e}, status=status.HTTP_400_BAD_REQUEST)
+    
