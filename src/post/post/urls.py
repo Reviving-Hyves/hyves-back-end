8ed80cc54e8ea0580107d3f446df5cb2d40545ee
For3vigt <nickwelles@live.nl>
2024-11-13 10:33:56 +0100

    feat: :test_tube: Added functionality for Sentry monitoring

--- src/post/post/urls.py
+++ src/post/post/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_post, delete_post, list_posts
+from .views import create_post, delete_post, list_posts, trigger_error
 
 urlpatterns = [
     path('post/create/', create_post, name='create_post'),
     path('post/delete/<int:post_id>', delete_post, name='delete_post'),
     path('post/list/', list_posts, name='list_posts'),
+    path('sentry-debug/', trigger_error),
 ]
