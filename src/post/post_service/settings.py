be8739408a8ebeec5bfa5031e7682ed20e923622
For3vigt <nickwelles@live.nl>
2024-12-13 11:17:41 +0100

    fix: :lock: Fix middleware to handle preflight requests correctly

--- src/post/post_service/settings.py
+++ src/post/post_service/settings.py
@@ -62,8 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'post.middleware.TokenAuthMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'post.middleware.TokenAuthMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
