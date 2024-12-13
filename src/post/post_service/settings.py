de432a760dd6f8aa4a1e589224448cf399c997b0
For3vigt <nickwelles@live.nl>
2024-12-13 11:13:37 +0100

    chore: :closed_lock_with_key: Add CORS urls to environment

--- src/post/post_service/settings.py
+++ src/post/post_service/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'post',
 ]
 
@@ -62,7 +63,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'post.middleware.TokenAuthMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+if env.str('F_PROD_HOST', default=''):
+    CORS_ALLOWED_ORIGINS.append('http://' + env.str('PROD_HOST'))
 
 ROOT_URLCONF = 'post_service.urls'
 
